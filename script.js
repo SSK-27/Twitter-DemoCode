ab22839aca71ca55a87d1c7ea6b5017526b8fe59
nick-hack <kavathekaraniket02@gmail.com>
2023-08-25 00:22:44 +0530

    Push the first changes on git

---
+++ script.js
@@ -0,0 +1,8 @@
+var tweetData = [];
+
+function addTweet(){
+    document.querySelector('#dataToShow').innerHTMLs = this.tweetData;
+    if(this.tweetData){
+        this.dataTweeted = true;
+    }
+}
